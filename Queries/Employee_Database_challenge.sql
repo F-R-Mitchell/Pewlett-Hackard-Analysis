b095e9e35e1f1c8765d239fdff71477cce5e030d
F-R-Mitchell <87910875+F-R-Mitchell@users.noreply.github.com>
2021-10-17 16:34:30 -0400

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,57 @@
+-- Info from employees table
+SELECT emp_no, first_name, last_name
+FROM employees;
+
+--Info from titles table
+SELECT title, from_date, to_date
+FROM titles;
+
+--Join employees and titles tables
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+--Retiring titles
+SELECT COUNT(title), title
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+
+--Deliverable 2
+--Employees table
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees
+
+--Dept employee table
+SELECT from_date, to_date
+FROM dept_emp
+
+--titles table
+SELECT title 
+FROM titles
+
+--Joining employees and dept employees tables
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+FULL OUTER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
